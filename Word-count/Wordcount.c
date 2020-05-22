6d60f10752c5d86027a3115b1faa6eda736cfd93
e <1181874971@qq.com>
2020-05-22 16:45:41 +0800

    commit again

--- Word-count/Wordcount.c
+++ Word-count/Wordcount.c
@@ -1,77 +1,49 @@
-//
-//#include <stdio.h>
-//#include <string.h>
-//int main()
-//{
-//	//char filename[20];
-//	//char word;
-//	//printf("[parameter] [input_file_name]/n");
-//	//scanf("%s",word);
-//	//
-//	//FILE *fp; // 指向文件的指针
-//	//char buffer[1003];
-//	//int bufferLen;
-//	//int i; //当前读到缓冲区的第i个字符
-//	//char c;
-//	//int isLastBlank = 0;// 上个字符是 否是空格
-//	//int charNum = 0;// 当前行的字符数
-//	//int wordNum = 0; //肖前行的单词数
-//
-//
-//	
-//	
-//	return 0;
-//}
-#include <fstream>
-#include <map>
-#include <string>
-using namespace std;
-
-bool isdigit(char ch)
+#include <stdio.h>
+#include <string.h>
+int main()
 {
-	return (ch >= ' ' && ch <= '9');
-}
-
-int main(void)
-{
-	map<string, int> words;  //转int类型
-	ifstream fin("input.txt");//要统计的文件
-	ofstream fout("output.txt");//结果存放在output中
-
-	string str;
-	int wordCount = 0;
-	string d;
-	int charCount = 0;
-
-
-	if (!fin || !fout)
-	{
-
-		exit(1);
+	char filename[20];
+	char words[100];
+	int isLastBlank = 0;
+	int i;  // 当前读到缓冲区的第i个字符
+	char c;  // 读取到的字符
+	int bufferLen;
+	int charNum = 0;  // 当前行的字符数
+	int wordNum = 0; // 当前行的单词数
+	int totalNum[2] = { 0, 0 };
+	
+	FILE *fp;
+	fp = fopen("textfile.txt", "rb");
+	if ((fp = fopen("textfile.txt", "rb")) == NULL) {
+		printf("读取文件失败\n");
 	}
-	while (fin.good())
-	{
-		fin >> str;
-		words[str]++;
-	}
-	fin.close();
-	fin.open("input.txt");
-	while (getline(fin, d)) //以行为单位读入文件
-		charCount += d.size(); //累计字符数
-	fin.close();
-
-	for (map<string, int>::iterator mit = words.begin(); mit != words.end(); ++mit)
-	{
-		if (!isdigit((mit->first)[0]))
-		{
-			fout << "<" << mit->first << ">" << ": " << mit->second << endl;
-			++wordCount;
+	else
+		printf("读取文件\n");
+	while (fgets(words, 1003, fp) != NULL) {
+		bufferLen = strlen(words);
+		// 遍历缓冲区的内容
+		for (i = 0; i < bufferLen; i++) {
+			c = words[i];
+			if (c == ' ' || c == ',') {  // 遇到空格,逗号
+				!isLastBlank && wordNum++;  // 如果上个字符不是空格，那么单词数加1
+				isLastBlank = 1;
+			}
+			else if (c != '\n'&&c != '\r') {  // 忽略换行符
+				charNum++;  //字符数加1
+				isLastBlank = 0;
+			}
 		}
+		!isLastBlank && wordNum++;  
+		isLastBlank = 1; 
+		totalNum[0] += charNum;  // 总字符数
+		totalNum[1] += wordNum;  // 总单词数
+		printf("%-7d%-7d\n",  wordNum, charNum);
+		charNum = 0;
+		wordNum = 0;
 	}
-	fout << "wordTotal: " << wordCount << endl;  // 文件输出单词个数
-	fout << "charTotal: " << charCount << endl;   //文件输出字符个数
-	fout.close();
-	printf("单词数：%d\n", wordCount);
-	printf("字符数：%d\n", charCount);
+	printf("单词数：%d\n", wordNum);
+	printf("字符数：%d\n", charNum);
+
+
 	return 0;
 }
